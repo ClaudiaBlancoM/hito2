19584dda49e781550c8caf89b4fed88fca8ae0cb
ClaudiaBlancoM <cabm0626@gmail.com>
2022-06-03 02:38:04 -0400

    creación modelos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :warehouse_registers
+  devise_for :sales
+  devise_for :products
+  devise_for :suppliers
+  devise_for :clients
+  devise_for :categories
+  devise_for :users
+  get 'home/index'
+  get 'home/faq'
+  root to: "home#index"
 end
